d9b94984167d54c3aeacbb001ba2b76d551de28f
VAleksandr <vlasovgomel@gmail.com>
2022-03-24 11:11:02 +0300

    changed index.js

--- HomeWork_5/index.js
+++ HomeWork_5/index.js
@@ -80,30 +80,35 @@ const makeAnOrder = (day, time) => {
   const arrPeriodsDay = openingHours.periods[newDay];
   const timeKitchen = 30;
   const orderStatus = [];
+
+  if (!!(newDay != -1)) {
   
-  for (let i = 0; i < arrPeriodsDay.length; i++) {
-    const fromTime = +arrPeriodsDay[i].from.split(':').join('');
-    const toTime = arrPeriodsDay[i].to.split(':');
-    
-    if (+toTime[1] <= timeKitchen) {
-      toTime[1] = 60 - (timeKitchen - +toTime[1]);
+    for (let i = 0; i < arrPeriodsDay.length; i++) {
+      const fromTime = +arrPeriodsDay[i].from.split(':').join('');
+      const toTime = arrPeriodsDay[i].to.split(':');
+      
+      if (+toTime[1] <= timeKitchen) {
+        toTime[1] = 60 - (timeKitchen - +toTime[1]);
 
-      +toTime[0] === 0 ?
-        toTime[0] = 24 - 1 :
-        toTime[0] = +toTime[0] - 1;
+        +toTime[0] === 0 ?
+          toTime[0] = 24 - 1 :
+          toTime[0] = +toTime[0] - 1;
 
-      if(+toTime[1] === 60) {
-        +toTime[0]++;
-        toTime[1] = '00';
-      }  
+        if(+toTime[1] === 60) {
+          +toTime[0]++;
+          toTime[1] = '00';
+        }  
+      }
+      
+      orderTime >= fromTime && orderTime < toTime.join('') ?
+      orderStatus.push('true') :
+      orderStatus.push('false'); 
     }
-    
-    orderTime >= fromTime && orderTime < toTime.join('') ?
-    orderStatus.push('true') :
-    orderStatus.push('false'); 
-  }
 
-  return !!orderStatus.find(index => index === 'true');
+    return !!orderStatus.find(index => index === 'true');
+  } else {
+    return false;
+  }
 }
 
 const now = moment();
