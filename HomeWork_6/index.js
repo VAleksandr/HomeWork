2e289e7f507117188a82fb91d664d449a05b0542
VAleksandr <vlasovgomel@gmail.com>
2022-03-28 23:54:31 +0300

    added files

---
+++ HomeWork_6/index.js
@@ -0,0 +1,43 @@
+const todos_container = document.getElementById('todos_container');
+
+const requestTodo = async () => {
+  for (let i = 1; i <= 30; i++) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${i}`)
+      .then(response => {
+        if(response.ok) {
+          response.json().then(result => outputTodo(result));
+        } 
+    });    
+  }
+}
+
+const outputTodo = todos => {
+  const todo_text = document.createElement('div');
+  const todo_id = document.createElement('p');
+  const todo_title = document.createElement('p');
+  const todo_completed = document.createElement('p');  
+  const todoDeleteBtn = document.createElement('button');
+  
+  todo_id.innerText = `id: ${todos.id}`;
+  todo_title.innerText = `title: ${todos.title}`;
+  todoDeleteBtn.innerText = 'Delete';
+
+  todo_text.classList.add('todo_text');
+  todoDeleteBtn.classList.add('todoDeleteBtn');
+  
+  todos.completed ? 
+    todo_completed.innerText = 'status: completed': 
+    todo_completed.innerText = 'status: in progress';
+  
+  todos_container.append(todo_text);
+  todo_text.append(todo_id, todo_title, todo_completed, todoDeleteBtn);
+  deleteTodo();
+}
+
+const deleteTodo = () => {
+  todos_container.querySelectorAll('button').forEach(todoDeleteBtn => { 
+    todoDeleteBtn.onclick = () => todoDeleteBtn.closest('div').remove();
+  }); 
+}
+
+requestTodo();    
